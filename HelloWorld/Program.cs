f3d445f680a222c7ffedc2922141e5860c267d90
Emre MERT <emremert117@gmail.com>
2021-06-01 21:20:40 +0300

    mesaj değiştirdim

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Merhaba Dünya!");
+            Console.WriteLine("Merhaba Dünya, hava bugün çok güzel!");
         }
     }
 }
